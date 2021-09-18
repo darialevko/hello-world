322664657d2d8250ff7de70a152496bca2557cf6
artem pushkar <artem.pushkar@harmonicinc.com>
2021-09-18 22:57:47 +0300

    new task

---
+++ 3.js
@@ -0,0 +1,5 @@
+// Split string and numbers into separated arrays
+// ignore any not string or number values
+// print into console results
+
+const source = ['car', 'tree', 34, 2, 'range rover', 'girl', {}, null, 54, 76, 'chevrolet'];
