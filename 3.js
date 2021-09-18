8c14e37341fbb81cac1804e819ea5172accbd4c6
darialevko <dashalevko93@gmail.com>
2021-09-18 23:23:44 +0300

    Task is done. Please review.

--- 3.js
+++ 3.js
@@ -7,3 +7,20 @@ const source = ['car', 'tree', 34, 2, 'range rover', 'girl', {}, null, 54, 76, '
 // tips
 // use typeof operator to understand what is what
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+var str = [];
+var num = [];
+
+for (var i=0; source.length > i; i++) {
+
+	if (typeof source[i] === 'number') {
+		num.push(source[i]);
+	} else if (typeof source[i] === 'string') {
+		str.push(source[i]);
+	} else {
+		continue;
+	}
+}
+
+console.log(num);
+console.log(str);
