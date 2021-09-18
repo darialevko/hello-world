f51a63c439e08ed867416702f32d1ac1ddcd8724
artem pushkar <artem.pushkar@harmonicinc.com>
2021-09-18 23:03:55 +0300

    new task

--- 3.js
+++ 3.js
@@ -3,3 +3,7 @@
 // print into console results
 
 const source = ['car', 'tree', 34, 2, 'range rover', 'girl', {}, null, 54, 76, 'chevrolet'];
+
+// tips
+// use typeof operator to understand what is what
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
