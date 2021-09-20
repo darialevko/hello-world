577a3ffd5381b62c74c25632b29243b9d6a1ab52
darialevko <dashalevko93@gmail.com>
2021-09-20 14:49:57 +0300

    Task 4 is done.Please review

--- js-tasks/4.js
+++ js-tasks/4.js
@@ -8,3 +8,31 @@
 *   var resultArray = [2, 6];
 *   => The sum of numbers is: 8;
 * */
+
+var arr = [];
+var newArr = [];
+var resultArray = [];
+var sum = 0;
+
+while (arr.length < 4) {
+	var r = Math.floor(Math.random() * 1000000 + 1);
+    arr.push(r);
+}
+newArr = arr;
+//console.log(newArr);
+
+for (var i=0; i < newArr.length; i++) {
+	if (newArr[i] % 2) {
+		continue;
+	} else {
+		resultArray.push(newArr[i]);
+	}
+}
+//console.log(resultArray);
+
+for (var s=0; s < resultArray.length; s++) {
+	sum+=resultArray[s];
+}
+
+console.log("The sum of numbers is: " + sum);
+
