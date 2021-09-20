181815cb5504177c9aafff9eb6fc5ee19e71cdd1
darialevko <dashalevko93@gmail.com>
2021-09-20 15:03:10 +0300

    Moved 5.js

---
+++ js-tasks/5.js
@@ -0,0 +1,24 @@
+<script>
+		var input = "javascript is awesome";
+		var output = "";
+		var x = "4";
+		var y = "3";
+		var z = "1";
+		var c = "0";
+
+		for (var i=0; input.length > i; i++) {
+			if (input[i] === "a") {
+				output+=x;
+			} else if (input[i] === "e") {
+				output+=y;
+			} else if (input[i] === "i") {
+				output+=z;
+			} else if (input[i] === "o") {
+				output+=c;
+			} else {
+				output+=input[i]
+			}
+		}
+		console.log(output);
+// result: j4v4scr1pt 1s 4w3s0m3
+	</script>
